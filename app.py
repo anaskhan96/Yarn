5b088b94c77e3953de08d59bc86861a9996b5baf
Anas Khan <anas.khan96@outlook.com>
2017-03-26 14:14:05 +0530

    formatted with autopep8

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 import json
 import check_genre
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method=='POST':
-		text=request.json['hello']
-		number=check_genre.execute(text)
-		return json.dumps({'hello':number},200,{'ContentType':'application/json'})
-	return render_template("index1.html")
+    if request.method == 'POST':
+        text = request.json['hello']
+        number = check_genre.execute(text)
+        return json.dumps({'hello': number}, 200, {'ContentType': 'application/json'})
+    return render_template("index1.html")
 
-if __name__=='__main__':
-	app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
